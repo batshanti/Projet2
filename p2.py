51a6dda5bb6cefbb6e483e4ba46165a3cbc7e6d0
batshanti <teddylouvet@yahoo.fr>
2021-08-23 22:19:42 +0200

    modification p2.py > utilisation des class

--- p2.py
+++ p2.py
@@ -1,5 +1,28 @@
 from scrap import *
+from csvv import *
+from book import *
+from category import *
 
 url = "http://books.toscrape.com/index.html"
 # Création d'une instance de la classe Scrap
 ScrapPage = Scrap(url)
+
+# Récupère toutes les urls de toutes les catégories dans un tableau et supprime le 1er élément
+all_url_category = ScrapPage.getCategories()
+del all_url_category[0]
+
+for lines in all_url_category:
+    newCat = Category(lines)
+    newCat.Set_name()
+    newCat.Set_urls()
+    newCat.Add_Book()
+    newCSV = Csv(newCat.name)
+    newCSV.write_head()
+    for line in newCat.books:
+        newBook = Book(line)
+        newBook.Get_book_infos()
+        liste_infos = [newBook.product_page_url, newBook.universal_product_code, newBook.title,
+                       newBook.price_excluding_tax, newBook.number_available, newBook.product_description,
+                       newBook.category, newBook.review_rating, newBook.image_url]
+        newCSV.write_book_infos(liste_infos)
+        newBook.get_image()
